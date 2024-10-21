e5f45c5a676c9aaf76df87642a95ccbea7f9c6a5
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2024-10-21 17:23:51 +0700

    fe: Include missing changes from previous commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { WeatherPage } from './pages/WeatherPage.tsx';
+import Nav from './components/common/Nav.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CoursePage from './pages/CoursePage.tsx';
+import { Separator } from './components/common/ui/separator.tsx';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <WeatherPage />
-    </div>
-  )
+    <Router>
+      <Nav className='max-w-full max-h-18' />
+      <Separator className='-mt-2'/>
+      <main className='flex-grow'>
+        <Routes>
+          <Route path='/' element={<WeatherPage />} />
+          <Route path='/course' element={<CoursePage />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
