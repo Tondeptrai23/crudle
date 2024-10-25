0655642beee508d203bf400132aca793e31b82fe
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-10-25 17:01:50 +0700

    be: Update StudentService and TeacherService
    - Add GetStudentByUserIdAsync method in StudentService to retrieve a student's information by user id.
    - Add GetTeacherByUserIdAsync method in TeacherService to retrieve a teacher's information by user id.

--- backend/Services/Interface/ITeacherService.cs
+++ backend/Services/Interface/ITeacherService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using _3w1m.Dtos.Teacher;
+using _3w1m.Models.Domain;
+using _3w1m.Models.Exceptions;
 
 namespace _3w1m.Services.Interface;
 public interface ITeacherService
@@ -19,7 +21,7 @@ public interface ITeacherService
     /// Retrieves a collection of all teachers.
     /// </summary>
     /// <returns>The task result contains a collection of teacher DTOs.</returns>
-    Task<(int count, IEnumerable<TeacherDto> teachers)> GetTeachersAsync(TeacherCollectionQueryDto queryDto);
+    Task<(int count, IEnumerable<TeacherDto> teachers)> GetTeachersAsync(TeacherCollectionQueryDto? queryDto);
 
     /// <summary>
     /// Creates a new teacher record.
@@ -36,4 +38,12 @@ public interface ITeacherService
     /// <returns>The task result contains the updated teacher's information.</returns>
     /// <exception cref="ResourceNotFoundException">Thrown when the teacher is not found.</exception>
     Task<TeacherDto> UpdateTeacherAsync(int teacherId, UpdateTeacherRequestDto teacherData);
+        
+    /// <summary>
+    /// Get a teacher by user id
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user</param>
+    /// <returns>The task contains information of teacher which match the userId</returns>
+    /// <exception cref="ResourceNotFoundException">Thrown when can not find the teacher matching the userId argument</exception>
+    Task<TeacherDto> GetTeacherByUserIdAsync(string userId);
 }
