34810b465ce4ec237910f62514c9890c2b12dcde
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-12-03 08:28:23 +0700

    fe: Refactor course page

--- frontend/src/pages/CoursePage.tsx
+++ frontend/src/pages/CoursePage.tsx
@@ -1,5 +1,6 @@
 import api from '@/utils/api';
 import { useQuery } from '@tanstack/react-query';
+import { useStudentCourses } from '@/hooks/api/useCourseApi';
 import { Navigate } from 'react-router-dom';
 import { CourseCard } from '@/components/course/CourseCard';
 import { Breadcrumb } from '@/components/common/ui/breadcrumb';
@@ -7,31 +8,10 @@ import React from 'react';
 import { CourseResponse } from '@/types/course';
 import Course from '@/types/course';
 import CourseSkeleton from '@/components/course/CourseSkeleton';
-
-const mapToCourse = (response: CourseResponse): Course => ({
-  id: response.CourseId.toString(),
-  name: response.Name,
-  description: response.Description,
-  code: response.Code,
-  startDate: response.StartDate,
-  teacherId: response.Teacher.TeacherId.toString(),
-  teacherName: response.Teacher.Fullname,
-});
-
-const fetchCourses = async (): Promise<CourseResponse[]> => {
-  const response = await api.get('/api/Student/Course');
-  return response.data.Data;
-};
+import { mapToCourse } from '@/services/CourseService';
 
 const CoursePage: React.FC = () => {
-  const {
-    data: courses,
-    isPending,
-    error,
-  } = useQuery({
-    queryKey: ['courses'],
-    queryFn: fetchCourses,
-  });
+  const { data: courses, isPending, error } = useStudentCourses();
 
   if (isPending)
     return (
