70a9c458dc7ba85765f1c45e1f29dff115456a24
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2024-12-17 16:36:21 +0700

    be: Small fix database

--- backend/Models/Domain/AssignmentSubmission.cs
+++ backend/Models/Domain/AssignmentSubmission.cs
@@ -4,7 +4,7 @@ public class AssignmentSubmission
 {
     public int SubmissionId { get; set; }
     
-    
+    public DateTime StartedAt { get; set; }
     public DateTime SubmittedAt { get; set; }
     
     public double Score { get; set; }
