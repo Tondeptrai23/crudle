5f7ac0023ecdad0f0b39e3dad8f178e71eb73415
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-12-18 16:25:50 +0700

    be: Add date range filters for article creation and update

--- backend/Dtos/Article/ArticleCollectionQueryDto.cs
+++ backend/Dtos/Article/ArticleCollectionQueryDto.cs
@@ -5,6 +5,8 @@ public class ArticleCollectionQueryDto: BaseCollectionQueryDto
     public string? Title { get; set; }
     public string? Summary { get; set; }
     public string? Content { get; set; }
-    public DateTime? CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    public DateTime? CreatedAtFrom { get; set; }
+    public DateTime? CreatedAtTo { get; set; }
+    public DateTime? UpdatedAtFrom { get; set; }
+    public DateTime? UpdatedAtTo { get; set; }
 }
