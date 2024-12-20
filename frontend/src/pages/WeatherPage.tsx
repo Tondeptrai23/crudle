f9bcd3a1cec8283dd3bc321e7f1fabac6933177a
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-12-20 20:27:33 +0700

    fe: Finalizing dashboard layout

--- frontend/src/pages/WeatherPage.tsx
+++ frontend/src/pages/WeatherPage.tsx
@@ -10,12 +10,9 @@ export const WeatherPage: React.FC = () => {
         Sidebar
       </div>
       <div className='container m-8 flex flex-col gap-4'>
-        <h2 className='mb-4 text-2xl font-semibold'>Upcoming events</h2>
+        <h2 className='text-2xl font-semibold'>Upcoming events</h2>
         <div className='flex flex-row gap-4'>
-          <div className='flex-1'>
-            There are no upcoming events.
-            {/* Add your events list here */}
-          </div>
+          <div className='flex-1'>There are no upcoming events.</div>
           <div className=''>
             <Calendar
               mode='single'
@@ -25,6 +22,40 @@ export const WeatherPage: React.FC = () => {
             />
           </div>
         </div>
+        <h2 className='text-2xl font-semibold'>Results</h2>
+        <table className='min-w-full divide-y divide-gray-200'>
+          <thead className='bg-gray-50'>
+            <tr>
+              <th
+                scope='col'
+                className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500'
+              >
+                Course ID
+              </th>
+              <th
+                scope='col'
+                className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500'
+              >
+                Course Name
+              </th>
+              <th
+                scope='col'
+                className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500'
+              >
+                Grade
+              </th>
+            </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-200 bg-white'>
+            <tr>
+              <td className='whitespace-nowrap px-6 py-4'>CS101</td>
+              <td className='whitespace-nowrap px-6 py-4'>
+                Introduction to Computer Science
+              </td>
+              <td className='whitespace-nowrap px-6 py-4'>A</td>
+            </tr>
+          </tbody>
+        </table>{' '}
       </div>
     </div>
   );
