be1254e7b451fd13a4d22d4e62a58222371d5f7b
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-12-21 22:42:41 +0700

    be: Add DTOs for assignment submissions and student answers

---
+++ backend/Dtos/AssignmentSubmission/AssignmentSubmissionMinimalDto.cs
@@ -0,0 +1,12 @@
+﻿namespace _3w1m.Dtos.Assignment;
+
+public class AssignmentSubmissionMinimalDto
+{
+    public int SubmissionId { get; set; }
+    public DateTime StartedAt { get; set; }
+    public DateTime SubmittedAt { get; set; }
+    public double? Score { get; set; }
+    public int AssignmentId { get; set; }
+    public int StudentId { get; set; }
+    public string StudentName { get; set; }
+}
---
+++ backend/Dtos/Question/QuestionWithAnswerForStudentDto.cs
@@ -0,0 +1,8 @@
+﻿using _3w1m.Dtos.Answers;
+
+namespace _3w1m.Dtos.Questions;
+
+public class QuestionWithAnswerForStudentDto: QuestionWithStudentAnswerDto
+{
+    public ICollection<StudentAnswerDto> StudentAnswers { get; set; }
+}
