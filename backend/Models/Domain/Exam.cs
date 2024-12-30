67a5a7d77b9a3e672bad3d47f77fa3c76ff9f649
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-12-30 10:07:46 +0700

    be: Add exam management entities and relationships in the database context

---
+++ backend/Models/Domain/Exam.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace _3w1m.Models.Domain;
+
+public class Exam
+{
+    public int ExamId { get; set; }
+    
+    public int CourseId { get; set; }
+    
+    public string Name { get; set; }   
+    
+    public string Content { get; set; }
+    
+    public int Duration { get; set; }
+    
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime UpdatedAt { get; set; }
+    
+    public ICollection<Question> Questions { get; set; }
+
+    public Course Course { get; set; }
+
+    public ICollection<ExamQuestion> ExamQuestions { get; set; }
+    
+    public ICollection<ExamSubmission> Submissions { get; set; }
+}
