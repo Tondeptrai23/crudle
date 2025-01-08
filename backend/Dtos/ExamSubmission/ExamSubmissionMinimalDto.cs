f23f176fff89049a3c83d6ce825ad811744334a5
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-01-08 22:32:26 +0700

    be: rename ExamSubmissionId to SubmissionId for improved clarity

--- backend/Dtos/ExamSubmission/ExamSubmissionMinimalDto.cs
+++ backend/Dtos/ExamSubmission/ExamSubmissionMinimalDto.cs
@@ -3,7 +3,7 @@
 public class ExamSubmissionMinimalDto
 {
     public int ExamId { get; set; } 
-    public int ExamSubmissionId { get; set; }
+    public int SubmissionId { get; set; }
     public string StudentName { get; set; }
     public DateTime StartedAt { get; set; }
     public DateTime? SubmittedAt { get; set; }
